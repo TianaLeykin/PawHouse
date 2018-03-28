d5f74a33680ac021b8d095258c19d72dbb43610c
tomer <tomerventura@mail.tau.ac.il>
2018-03-28 16:50:39 +0300

    Added new activity for testing

---
+++ app/src/main/java/com/pawhouseteam/googleworkshop2018/tau/pawhouse/TomerTestLayout.java
@@ -0,0 +1,13 @@
+package com.pawhouseteam.googleworkshop2018.tau.pawhouse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TomerTestLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tomer_test_layout);
+    }
+}
