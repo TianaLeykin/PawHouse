dfe152987c705758259fbf8b80e4c3cb87c37f59
tomer <tomerventura@mail.tau.ac.il>
2018-03-28 18:11:32 +0300

    Added new activity for testing

--- app/src/main/java/com/pawhouseteam/googleworkshop2018/tau/pawhouse/TomerTestLayout.java
+++ app/src/main/java/com/pawhouseteam/googleworkshop2018/tau/pawhouse/TomerTestLayout.java
@@ -2,12 +2,34 @@ package com.pawhouseteam.googleworkshop2018.tau.pawhouse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class TomerTestLayout extends AppCompatActivity {
+
+    private Button buttonSingUp,buttonSingIn;
+    private EditText editTextName;
+    private EditText editTextPasssword;
+    private TextView viewTextInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tomer_test_layout);
+
+        buttonSingUp = (Button) findViewById(R.id.buttonSingUp);
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        editTextPasssword = (EditText) findViewById(R.id.editTextPassword);
+        viewTextInfo = (TextView) findViewById(R.id.textViewInfo);
+
+    }
+
+    public void clickSingUp(View view) {
+        viewTextInfo.setText("good very good");
+    }
+
+    public void clickSingIn(View view) {
     }
 }
