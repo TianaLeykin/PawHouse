578aaeba49dbf9c046bcc4fb0ac6956639b87cc2
tamirfind <tamirfind@gmail.com>
2018-04-01 12:28:53 +0300

    added connection to fire base and dependencies

--- app/src/main/java/com/pawhouseteam/googleworkshop2018/tau/pawhouse/MainActivity.java
+++ app/src/main/java/com/pawhouseteam/googleworkshop2018/tau/pawhouse/MainActivity.java
@@ -3,6 +3,9 @@ package com.pawhouseteam.googleworkshop2018.tau.pawhouse;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
